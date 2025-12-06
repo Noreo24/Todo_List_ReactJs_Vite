bed1356f5e6ce79dcdac791a55c2c2de6378bfa2
Tuấn Bùi Anh <buianhtuan.kbhb24@gmail.com>
2025-12-07 00:33:15 +0700

    Create register page use Form Antd - 00h33 07/12/2025

--- FE/src/pages/register.jsx
+++ FE/src/pages/register.jsx
@@ -1,6 +1,52 @@
+import { Button, Form, Input } from "antd";
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log("Success:", values);
+    }
     return (
-        <div>Register Page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        >
+            <div style={{ margin: "50px" }}>
+                <Form.Item
+                    label="Full name"
+                    name="fullname"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone"
+                    name="phone"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <div>
+                    <Button onClick={() => form.submit()} type="primary">Register</Button>
+                </div>
+            </div >
+        </Form >
     );
 }
 
